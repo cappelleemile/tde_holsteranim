0226fb3afb213a7af96b405b8d9a99584558b452
TDE Mods <153952211+tdemods@users.noreply.github.com>
2025-01-21 21:09:34 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'TDE Mods'
+description 'Alt Key Holster Animation'
+version '1.0.0'
+
+client_script "client.lua"
+shared_Script "config.lua"
